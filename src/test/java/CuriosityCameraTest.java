0aa9ca2925071a70afff02e003de5f36f6b2ac01
mikhail.zlochevskyi <mikhail.zlochevskyi@bonial.com>
2017-08-29 21:33:14 +0300

    set up testng and stub test

--- src/test/java/CuriosityCameraTest.java
+++ src/test/java/CuriosityCameraTest.java
@@ -1,5 +1,24 @@
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 /**
  * Created by mikhail_z on 8/29/17.
  */
 public class CuriosityCameraTest {
+
+    @BeforeClass
+    public void setUp() {
+
+    }
+
+    @Test
+    public void compareImages_Sol_EarthDate(){
+
+    }
+
+    @AfterClass
+    public void cleanUp(){
+
+    }
 }
