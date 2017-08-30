5c2a7e600673aa69c8074e9570dd7104b357e898
mikhail.zlochevskyi <mikhail.zlochevskyi@bonial.com>
2017-08-30 08:47:29 +0300

    create comparePhotos_Sol_EarthDate test [LOW PERFORMANCE]

--- src/test/java/CuriosityCameraTest.java
+++ src/test/java/CuriosityCameraTest.java
@@ -2,13 +2,12 @@ import io.restassured.RestAssured;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
-import static io.restassured.RestAssured.given;
 
-/**
- * Created by mikhail_z on 8/29/17.
- */
 public class CuriosityCameraTest {
 
     public static final String API_KEY = "2wyfEESO8DP2s5tqjsDMBbORVCOy1X4Fv4FKb4XC";
@@ -17,22 +16,28 @@ public class CuriosityCameraTest {
     public static final int SOL = 1000;
     public static final String EARTH_DATE = "2015-5-30";
 
+    public static final int PHOTOS_CNT = 10;
+
+
     @BeforeClass
     public void setUp() {
         RestAssured.baseURI = BASE_REQ_URL;
     }
 
     @Test
-    public void compareImages_Sol_EarthDate(){
-        System.out.println(given().when()
-                .get("?sol=" + SOL + "&api_key=" + API_KEY).then()
-                .assertThat().body("photos[0].id", equalTo(102693)));
+    public void comparePhotos_Sol_EarthDate() {
+        for (int i = 0; i < PHOTOS_CNT; i++) {
+            given().when()
+                    .get("?earth_date=" + EARTH_DATE + "&api_key=" + API_KEY).then()
+                    .assertThat().body("photos[" + i + "]", equalTo(
+                            get("?sol=" + SOL + "&api_key=" + API_KEY).path("photos[" + i + "]"))
+                    );
 
-        given().when().get("?sol=" + SOL + "&api_key=" + API_KEY).then().statusCode(200);
+        }
     }
 
     @AfterClass
-    public void cleanUp(){
+    public void cleanUp() {
 
     }
 }
